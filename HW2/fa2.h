834ba20c6d925b0dba2d3be16fd446f422d4fc3b
Arsh Chauhan <achauhan@alaska.edu>
2016-02-15 11:55:55 -0900

    Passing all test for Exercise A

--- HW2/fa2.h
+++ HW2/fa2.h
@@ -3,13 +3,23 @@
 
 #include<vector>
 using std::vector;
+#include<random>
+using std::normal_distribution;
+using std::mt19937;
 #include<functional>
 using std::function;
 
 inline vector<double> normRand (size_t n)
 {
-	vector<double> randNos(n);
-	return randNos; //Dummy return
+	vector<double> randNos;
+	mt19937 gen (10); // Seed with constant -> we get same numbers at every run
+	normal_distribution <double> dist(100.0,15.0);
+	for(int i = 0; i < n; i++)
+	{
+		randNos.push_back(dist(gen));
+	}
+
+	return randNos;
 }
 
 inline vector<int> asyncSquares (size_t n)
@@ -23,21 +33,5 @@ auto repeatFunction (fn f, int n)
 {
 	return f;
 }
-
-/*int repeatFunction (std::function<int(int)> f , int n)
-{
-	return f(1);
-}*/
-
-//template<typename T>
-/*inline int operator() (std::function<int(int num) f, int n) repeatFunction
-{
-	return f(num); //Dummy return 
-}
-*/
-/*function<function<int(int)>f,int n >repeatFunction
-{
-	return f(n);
-}*/
 
 #endif
